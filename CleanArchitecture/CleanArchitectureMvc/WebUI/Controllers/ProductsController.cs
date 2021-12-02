50810a38b6c7b7b7dd4da74abc6ac2e4ba57501a
Rafael Vastag <rgvastag@gmail.com>
2021-12-02 17:51:50 -0300

    product controller

---
+++ CleanArchitecture/CleanArchitectureMvc/WebUI/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using Application.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productService.FindAllAsync();
+            return View(products);
+        }
+    }
+}
