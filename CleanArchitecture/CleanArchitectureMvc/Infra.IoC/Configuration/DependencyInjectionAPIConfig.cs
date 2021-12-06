a66a57b4198e1a204433b2d0adc0a285c804c551
Rafael Vastag <rgvastag@gmail.com>
2021-12-06 12:45:56 -0300

    dependency inject config

---
+++ CleanArchitecture/CleanArchitectureMvc/Infra.IoC/Configuration/DependencyInjectionAPIConfig.cs
@@ -0,0 +1,47 @@
+﻿using Application.Mappings;
+using Application.Services;
+using Application.Services.Impl;
+using Domain.Authentication;
+using Domain.Interfaces;
+using Infra.Data.Context;
+using Infra.Data.Identity;
+using Infra.Data.Repositories;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Infra.IoC.Configuration
+{
+    public static class DependencyInjectionAPIConfig
+    {
+        public static IServiceCollection AddInfrastructureAPI(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var myHandlers = AppDomain.CurrentDomain.Load("Application");
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+             b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName))
+            );
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddScoped<IAuthenticateService, AuthenticateService>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddMediatR(myHandlers);
+
+            services.AddAutoMapper(typeof(EntityToDTOMappingProfile));
+            services.AddAutoMapper(typeof(DTOToCommandMappingProfile));
+
+            return services;
+        }
+    }
+}
--- CleanArchitecture/CleanArchitectureMvc/WebAPI/Startup.cs
+++ CleanArchitecture/CleanArchitectureMvc/WebAPI/Startup.cs
@@ -1,16 +1,10 @@
+using Infra.IoC.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace WebAPI
 {
@@ -26,7 +20,7 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddInfrastructureAPI(Configuration);
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
